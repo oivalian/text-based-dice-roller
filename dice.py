b82ae088393b6325b9fe83213c2af66a9a96c6d1
oivalian <109859213+oivalian@users.noreply.github.com>
2023-12-29 23:57:41 +1100

    Update dice to actual dice (yay for unicode)

--- dice.py
+++ dice.py
@@ -4,8 +4,8 @@ while True:
     try:
         die_count = int(input("How many dice will you roll? >>> "))
         for _ in range(die_count):
-            dice = (1, 2, 3, 4, 5, 6)
-            print('|', int(r.choice(dice)), '|', end='')
+            dice = ("⚀", "⚁", "⚂", "⚃", "⚄", "⚅")
+            print(r.choice(dice), end='')
         print('\n')
     except ValueError:
         print('')
